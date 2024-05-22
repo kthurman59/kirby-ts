046fb5d80473a4d6d695ecf048b978322f9de8f3
kthurman59 <kthurman59@gmail.com>
2024-05-22 15:59:39 -0500

    feature: making map

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,9 @@
 // Making the map
+import { KaboomCtx } from "kaboom";
+import { scale } from "./constant";
+
+export async function makeMap(k: KaboomCtx, name: string) {
+    const mapData = await(await fetch(`./${name}.json`)).json(); 
+
+    const map = k.make([k.sprite(name)])
+}
