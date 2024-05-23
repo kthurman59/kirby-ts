7bdec8e608a820212742697146f0883f0dbe23cd
kthurman59 <kthurman59@gmail.com>
2024-05-22 20:23:47 -0500

    update

--- src/utils.ts
+++ src/utils.ts
@@ -5,5 +5,20 @@ import { scale } from "./constant";
 export async function makeMap(k: KaboomCtx, name: string) {
     const mapData = await(await fetch(`./${name}.json`)).json(); 
 
-    const map = k.make([k.sprite(name)])
+    const map = k.make([k.sprite(name), k.scale(scale), k.pos(0)])
+
+    const spawnPoints: { [key: string]: { x: number, y: number } } = {};
+
+    for (const layer of mapData.layers) {
+        if(layer.name === "colliders") {
+          for(const collider of layer.objects) {
+            map.add([
+                k.area({
+                    shape: new k.Rect(k.vec2(0), collider.width, collider.height),
+                    collisionIgnore: ["platform", "exit"],
+                }),
+            ]);
+          }
+        }
+    }
 }
